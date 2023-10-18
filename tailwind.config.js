98ea1b8f6c484aea20b545673dbb507009a2a1cf
abizarhaikal <abizarhaikal@gmail.com>
2023-10-18 18:48:12 +0700

    menu home ini menu awal web kita

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         'navbar-color':'rgba(8, 5, 2, 0.8)',
         'primary': 'rgba(8, 5, 2, 1)',
         'textPrimary': 'rgba(245, 235, 224, 1)',
+        'buttonSecondary': 'rgba(217, 217, 217, 0.2)',
+        'footer': 'rgba(39, 18, 58, 1)'
       },
       fontFamily: {
         primaryFont: ['Montserrat']
